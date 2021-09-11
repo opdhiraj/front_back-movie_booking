cab946a7a76819634d04eec532233eb563dd4791
op dhiraj <o.p.dhiraj@gmail.com>
2021-09-11 13:28:56 +0530

    checkpoint 3

--- moviebooking/server.js
+++ moviebooking/server.js
@@ -4,6 +4,16 @@ const PORT = 3000;
 app.get("/", (req, res) => {
   res.send("<h1>welcome</h1>");
 });
+app.get("/movies", (req, res) => {
+  res.send("All Movies Data in JSON format from Mongo DB");
+  r;
+});
+app.get("/genres", (req, res) => {
+  res.send("All Genres Data in JSON format from Mongo DB");
+});
+app.get("/artists", (req, res) => {
+  res.send("All Artists Data in JSON format from Mongo DB");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listenning at ${PORT}`);
